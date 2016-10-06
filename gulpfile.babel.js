5a0f889873fbe701c019b7354cf4584e347db930
sedano <alejandro@sedano.me>
2016-10-06 23:21:31 +0200

    Fix gulp.src script order

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -109,9 +109,9 @@ gulp.task('scripts', () =>
       // Note: Since we are not using useref in the scripts build pipeline,
       //       you need to explicitly list your scripts here in the right order
       //       to be correctly concatenated
-      './app/scripts/bug.ext.js',
       './app/scripts/jquery.mobile.custom.ext.js',
-      './app/scripts/main.js'
+      './app/scripts/main.js',
+      './app/scripts/bug.ext.js'
       // Other scripts
     ])
       .pipe($.newer('.tmp/scripts'))
