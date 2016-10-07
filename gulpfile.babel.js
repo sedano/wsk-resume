7bcdfa23585f73796c4f154e89452486726a6af8
sedano <alejandro@sedano.me>
2016-10-07 11:31:12 +0200

    Add open graph meta tags

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -206,7 +206,7 @@ gulp.task('default', ['clean'], cb =>
 // Run PageSpeed Insights
 gulp.task('pagespeed', cb =>
   // Update the below URL to the public URL of your site
-  pagespeed('example.com', {
+  pagespeed('sedano.me', {
     strategy: 'mobile'
     // By default we use the PageSpeed Insights free (no API key) tier.
     // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
